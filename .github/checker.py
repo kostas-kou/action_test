807fc3f9dd953a06406db8e09686f277500a8b31
Kostas Koumpouras <kkoumpouras@gmail.com>
2021-01-20 17:36:22 +0100

    Update checker

--- .github/checker.py
+++ .github/checker.py
@@ -2,8 +2,22 @@
 
 import yaml
 
-with open('postgresql-chart/values.yaml') as f:
-#with open('postgresql-chart/values_test.yaml') as f:
+# Class for coloured output in terminal
+class colours:
+    reset='\033[0m'
+    # Frontground
+    class fg:
+        red='\033[31m'
+        green='\033[32m'
+        cyan='\033[36m'
+        yellow='\033[93m'
+    # Background
+    class bg:
+        red='\033[41m'
+        green='\033[42m'
+
+#with open('postgresql-chart/values.yaml') as f:
+with open('../postgresql-chart/values.yaml') as f:
     md_dictionary = {}
     data = yaml.load(f, Loader=yaml.FullLoader)
     for k, v  in data.items():
@@ -30,9 +44,14 @@ with open('postgresql-chart/values.yaml') as f:
             md_dictionary.update({key:value})
 
 # Load the file into file_content
-file_content = [ line for line in open('postgresql-chart/README.md') ]
+file_content = [ line for line in open('../postgresql-chart/README.md') ]
 #file_content = [ line for line in open('postgresql-chart/README_test.md') ]
 
+# Create list for parameters that need to be changed or missing
+parameters_modify = []
+parameters_missing = []
+
+# Loop for checking if the changes made in values.yaml have been made also in README
 for k_all in md_dictionary:
     md_key = "`" + k_all + "`"
     if md_dictionary[k_all] == None:
@@ -66,7 +85,21 @@ for k_all in md_dictionary:
             elif md_value == None and "nil" in line_split[-1]:
                 continue
             else:
-                raise ValueError("change the value of " + md_key + " in README.md")
+                parameters_modify.append(md_key)
+                #raise ValueError("change the value of " + md_key + " in README.md")
         if len(file_content) == i and missing_in_readme:
-            raise Exception(md_key + " does not exist in README")
-print("README is synchronized with the parameters in postgresql-chart/values.yaml")
+            parameters_missing.append(md_key)
+            #raise Exception(md_key + " does not exist in README")
+
+if len(parameters_missing) == 0 and len(parameters_modify) == 0: 
+    print("README is synchronized with the parameters in postgresql-chart/values.yaml")
+else:
+    if len(parameters_modify) != 0:
+        print(colours.fg.red, "Values need to be changed in README:", colours.reset)
+        for modify in parameters_modify:
+            print(colours.fg.yellow, modify, colours.reset)
+    if len(parameters_missing) != 0:
+        print("Parameters missing from README file:")
+        for missing in parameters_missing:
+            print(colours.fg.yellow, missing, colours.reset)
+    raise ValueError("Make all the above changes in README.md")
